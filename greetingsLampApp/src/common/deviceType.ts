1cad9071244a1bdc2b109a5dcb899c709f704f2f
aarabmed <aarab.mohamed90@gmail.com>
2022-04-13 00:26:57 +0000

    add support to ios video format

--- greetingsLampApp/src/common/deviceType.ts
+++ greetingsLampApp/src/common/deviceType.ts
@@ -15,7 +15,7 @@ export function DeviceType(): string {
          return className!=='tablete'? setClassname('tablete'):null
       }
       if(width>1024){
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
           return  className!=='tablete'?setClassname('tablete'):null
         }
         else return setClassname('')
